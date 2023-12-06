a2d450757de71439e0ee7b58b66aaf1e919f98be
tindaking <82101177+TinDaKing@users.noreply.github.com>
2023-12-06 21:50:09 +0700

    style layout

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,14 +1,36 @@
 import React from "react";
 import Add from "../img/addAvatar.png";
-
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 const Register = () => {
-    return (
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const displayName = e.target[0].value;
+        const email = e.target[1].value;
+        const password = e.target[2].value;
+        const avatar = e.target[3].files[0];
 
+
+        const auth = getAuth();
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed up 
+                const user = userCredential.user;
+                // ...
+                console.log(user);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ..
+            });
+    }
+    return (
         <div className="formContainer">
             <div className="formWrapper">
                 <span className="logo">Chat app</span>
                 <span className="title">Register</span>
-                <form >
+                <form onSubmit={handleSubmit}>
                     <input type="text" placeholder="username" />
                     <input type="email" placeholder="email" />
                     <input type="password" placeholder="password" />
