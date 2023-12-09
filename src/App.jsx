1cd5e451673cfb8803710b7ccf52a8e88f69a571
tindaking <82101177+TinDaKing@users.noreply.github.com>
2023-12-09 23:18:23 +0700

    get some troubles..

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,13 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+
 function App() {
+  const { currentUser } = useContext(AuthContext)
+  console.log(currentUser)
+
   return (
     <BrowserRouter>
       <Routes>
