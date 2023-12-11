72c865eea5dab063b5fecec3f46b79f3d535b822
tindaking <82101177+TinDaKing@users.noreply.github.com>
2023-12-11 21:42:05 +0700

    Add useContext hook and update Chat component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import Cam from "../img/cam.png";
 import Add from "../img/add.png";
 import More from "../img/more.png";
-import Message from "./Message";
 import Messages from "./Messages";
 import Input from "./Input";
+import { ChatContext } from "../context/ChatContext";
 
 const Chat = () => {
+    const { data } = useContext(ChatContext);
+
     return (
         <div className="chat">
             <div className="chatInfo">
-                <span>Jane</span>
+                <span>{data.user?.displayName}</span>
                 <div className="chatIcons">
                     <img src={Cam} alt="" />
                     <img src={Add} alt="" />
@@ -21,6 +23,6 @@ const Chat = () => {
             <Input />
         </div>
     );
-}
+};
 
 export default Chat;
